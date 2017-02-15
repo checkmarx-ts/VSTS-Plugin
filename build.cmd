9eadfe3a89d1a6720c496b53cfe52e1b8fc80ca2
iland <ilan.dayan@checkmarx.com>
2017-02-15 15:58:23 +0200

    bugid: modified 'default branch' in rest response. CR_by: dorg

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 del *.vsix
 #tsc
 tfx extension create --manifest-globs vss-extension.json
-#tfx extension publish --share-with Checkmarx --token p22v2tzawa6nic7x6odxj2letrch4cxx6ikob7owufupg2bkxpfq
+#tfx extension publish --share-with Checkmarx --token x6msals6q6s7joqejd3od63xadccjfa374p5br7moorlmlhgwyra
 echo | pause
