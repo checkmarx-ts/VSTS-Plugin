092e2fc8a87d8ef4d47864e06ca4abc49fe3160b
alexeyk <alexey.kononov@checkmarx.com>
2019-11-06 14:41:17 +0200

    AB#174: Added more unit tests for the filtering.

--- CxScan/tests/filePathFilter.test.ts
+++ CxScan/tests/filePathFilter.test.ts
@@ -2,15 +2,63 @@ import * as assert from 'assert';
 import {FilePathFilter} from "../services/filePathFilter";
 
 describe('FilePathFilter', function () {
-    it('should include all paths if the filter string is empty', function() {
-        const target = new FilePathFilter('');
-        const paths = [
-            'basePage.java',
-            '.classpath',
-            'noextension'
-        ];
-        paths.forEach(path => {
-            assert.ok(target.includes(path), `'${path}' should be included.`);
-        });
+    type FilterTestCase = [string, boolean];
+
+    it('should include all files if the filter string is empty', function () {
+        verifyInclusion('', [
+            ['basePage.java', true],
+            ['.classpath', true],
+            ['noextension', true],
+        ]);
     });
+
+    it('should support including only files of certain types', function () {
+        verifyInclusion('*.java,*.js', [
+            ['basePage.java', true],
+            ['myscript.js', true],
+            ['myimage.png', false],
+            ['noext', false],
+        ]);
+    });
+
+    it('should support including all files except specified ones', function () {
+        verifyInclusion('!*.png,!*.class', [
+            ['myimage.png', false],
+            ['basePage.class', false],
+            ['basePage.java', true],
+            ['myscript.js', true],
+            ['noext', true],
+            ['.classpath', true],
+        ]);
+    });
+
+    it('should ignore leading and trailing whitespace in patterns', function () {
+        verifyInclusion('   *.java  ,      *.js ', [
+            ['basePage.java', true],
+            ['myscript.js', true],
+            ['myimage.png', false],
+            ['noext', false],
+        ]);
+    });
+
+    it('should ignore empty patterns', function () {
+        verifyInclusion('*.java,  ,, , *.js', [
+            ['basePage.java', true],
+            ['myscript.js', true],
+            ['myimage.png', false],
+            ['noext', false],
+        ]);
+    });
+
+    function verifyInclusion(filterString: string, testCases: FilterTestCase[]) {
+        const filter = new FilePathFilter(filterString);
+
+        testCases.forEach(pathTestCase => {
+            const path = pathTestCase[0];
+            const shouldBeIncluded = pathTestCase[1];
+
+            const assertion = shouldBeIncluded ? 'included' : 'excluded';
+            assert.equal(filter.includes(path), shouldBeIncluded, `'${path}' should be ${assertion} by the filter string: '${filterString}'`);
+        });
+    }
 });
