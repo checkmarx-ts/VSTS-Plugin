c5c0cdc9f57eff464801230ef54d28ed11020fce
AlexeyK <alexey.kononov@checkmarx.com>
2019-12-30 14:30:14 +0200

    Added threshold error test.

--- CxScan/tests/scanSummaryEvaluator.test.ts
+++ CxScan/tests/scanSummaryEvaluator.test.ts
@@ -9,17 +9,40 @@ describe("ScanSummaryEvaluator", function () {
         const config = getScanConfig();
         config.enablePolicyViolations = true;
 
-        const scanResults = new ScanResults(config);
-        scanResults.sastPolicies = ['policy1', 'policy2'];
-
         const logger = getDummyLogger();
 
         const target = new ScanSummaryEvaluator(config, logger, true);
+
+        const scanResults = new ScanResults(config);
+        scanResults.sastPolicies = ['policy1', 'policy2'];
         const summary = target.getScanSummary(scanResults);
+
         assert.ok(summary);
+        assert.ok(summary.hasErrors());
         assert.ok(summary.policyCheck);
         assert.ok(summary.policyCheck.wasPerformed);
         assert.deepStrictEqual(summary.policyCheck.violatedPolicyNames, scanResults.sastPolicies);
+    });
+
+    it('should return threshold errors in summary', function () {
+        const config = getScanConfig();
+        config.highThreshold = 1;
+        config.mediumThreshold = 5;
+        config.lowThreshold = 10;
+        config.vulnerabilityThreshold = true;
+
+        const logger = getDummyLogger();
+        const target = new ScanSummaryEvaluator(config, logger, false);
+
+        const scanResults = new ScanResults(config);
+        scanResults.highResults = 3;
+        scanResults.mediumResults = 8;
+        scanResults.lowResults = 4;
+        const summary = target.getScanSummary(scanResults);
+        assert.ok(summary);
+        assert.ok(summary.hasErrors());
+        assert.ok(summary.thresholdErrors);
+        assert.equal(summary.thresholdErrors.length, 2)
     });
 });
 
