20723001c3530e0911f172fe0a44e93b8696a359
Majd <majd.mahajenah@checkmarx.com>
2020-02-11 09:42:59 +0200

    fist commit

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -5,5 +5,4 @@ popd
 pushd %~dp0..
 del %~dp0..\*.vsix
 call tfx extension create --manifest-globs vss-extension.json
-REM tfx extension publish --share-with Checkmarx --token x6msals6q6s7joqejd3od63xadccjfa374p5br7moorlmlhgwyra
 popd
