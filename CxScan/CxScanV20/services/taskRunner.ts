4f8683b090330f7ad9e39300a78331674c0ff559
Majd <majd.mahajenah@checkmarx.com>
2020-06-28 15:42:08 +0300

    Save SCA reports, And A full Report Of SAST and SCA and fix include exclude problem AB#885

--- CxScan/CxScanV20/services/taskRunner.ts
+++ CxScan/CxScanV20/services/taskRunner.ts
@@ -8,9 +8,15 @@ import {ScanResults} from "@checkmarx/cx-common-js-client";
 import {TaskSkippedError} from "@checkmarx/cx-common-js-client";
 import {Logger} from "@checkmarx/cx-common-js-client";
 import {ScaClient} from "@checkmarx/cx-common-js-client/dist/services/clients/scaClient";
+import * as path from "path";
 
 export class TaskRunner {
     private static readonly REPORT_ATTACHMENT_NAME = 'cxReport';
+    private static readonly REPORT_SCA_PACKAGES = 'cxSCAPackages';
+    private static readonly REPORT_SCA_FINDINGS = 'cxSCAVulnerabilities';
+    private static readonly REPORT_SCA_SUMMARY = 'cxSCASummary';
+    private static readonly CxSAST = 'SAST';
+    private static readonly CxDependency = 'SCA';
 
     private readonly log: Logger = new ConsoleLogger();
 
@@ -28,11 +34,11 @@ export class TaskRunner {
          ...
     */
     async run() {
+        const errorMessage = "cannot be completed";
         try {
             this.printHeader();
 
             this.log.info('Entering CxScanner...');
-
             const reader = new ConfigReader(this.log);
             const config = reader.readConfig();
 
@@ -47,8 +53,14 @@ export class TaskRunner {
             if (err instanceof TaskSkippedError) {
                 taskLib.setResult(taskLib.TaskResult.Skipped, err.message);
             } else if (err instanceof Error) {
-                this.log.error(`Scan cannot be completed. ${err.stack}`);
-                taskLib.setResult(taskLib.TaskResult.Failed, `Scan cannot be completed. ${err.message}`);
+                if(err.message.includes(errorMessage)){
+                    this.log.error(err.message);
+                    taskLib.setResult(taskLib.TaskResult.Failed, err.message);
+                }else{
+                    this.log.error(`Scan cannot be completed. ${err.message}`);
+                    taskLib.setResult(taskLib.TaskResult.Failed, `Scan cannot be completed. ${err.message}`);
+                }
+
             } else {
                 taskLib.setResult(taskLib.TaskResult.Failed, `Scan cannot be completed. ${err}`);
             }
@@ -56,12 +68,43 @@ export class TaskRunner {
     }
 
     private async attachJsonReport(scanResults: ScanResults) {
-        const jsonReportPath = TaskRunner.generateJsonReportPath();
-        const reportJson = JSON.stringify(scanResults);
+        const jsonReportPath = TaskRunner.generateJsonReportPath(TaskRunner.REPORT_ATTACHMENT_NAME);
 
-        this.log.debug(`Writing report to ${jsonReportPath}`);
+        const reportJson = JSON.stringify(scanResults);
+        let scaPackages ='';
+        let scaFindings ='';
+        let scaSummary ='';
+        let scaPackagesPath = '';
+        let scaFindingsPath = '';
+        let scaSummaryPath = '';
+        if(scanResults.scaResults){
+            scaPackages = JSON.stringify(scanResults.scaResults.packages);
+            scaFindings = JSON.stringify(scanResults.scaResults.dependencyHighCVEReportTable.concat(scanResults.scaResults.dependencyMediumCVEReportTable,scanResults.scaResults.dependencyLowCVEReportTable));
+            scaSummary = JSON.stringify(scanResults.scaResults.summary)
+            scaPackagesPath= TaskRunner.generateJsonReportPath(TaskRunner.REPORT_SCA_PACKAGES);
+            scaFindingsPath= TaskRunner.generateJsonReportPath(TaskRunner.REPORT_SCA_FINDINGS);
+            scaSummaryPath= TaskRunner.generateJsonReportPath(TaskRunner.REPORT_SCA_SUMMARY);
+        }
+
+
+        await this.writeReportFile(jsonReportPath,reportJson);
+        taskLib.addAttachment(TaskRunner.REPORT_ATTACHMENT_NAME, TaskRunner.REPORT_ATTACHMENT_NAME, jsonReportPath);
+
+        if(scanResults.scaResults){
+            await this.writeReportFile(scaPackagesPath,scaPackages);
+            taskLib.addAttachment(TaskRunner.REPORT_SCA_PACKAGES, TaskRunner.REPORT_SCA_PACKAGES, scaPackagesPath);
+            await this.writeReportFile(scaSummaryPath,scaSummary);
+            taskLib.addAttachment(TaskRunner.REPORT_SCA_SUMMARY, TaskRunner.REPORT_SCA_SUMMARY, scaSummaryPath);
+            await this.writeReportFile(scaFindingsPath,scaFindings);
+            taskLib.addAttachment(TaskRunner.REPORT_SCA_FINDINGS, TaskRunner.REPORT_SCA_FINDINGS,scaFindingsPath);
+        }
+        this.log.info('Generated Checkmarx summary results.');
+    }
+
+    private async writeReportFile(jsonReportPath:string,jsonReport:string){
+        this.log.info(`Writing report to ${jsonReportPath}`);
         await new Promise((resolve, reject) => {
-            fs.writeFile(jsonReportPath, reportJson, err => {
+            fs.writeFile(jsonReportPath, jsonReport, err => {
                 if (err) {
                     reject(err);
                 } else {
@@ -69,19 +112,37 @@ export class TaskRunner {
                 }
             });
         });
-
-        taskLib.addAttachment(TaskRunner.REPORT_ATTACHMENT_NAME, TaskRunner.REPORT_ATTACHMENT_NAME, jsonReportPath);
-        this.log.info('Generated Checkmarx summary results.');
     }
 
-    private static generateJsonReportPath() {
+    private static generateJsonReportPath(reportType : string) {
         // A temporary folder that is cleaned after each pipeline run, so we don't have to remove
         // temp files manually.
-        const tempDir = taskLib.getVariable('Agent.TempDirectory');
+        let buildDir = taskLib.getVariable('Agent.BuildDirectory');
+        let buildNumber = taskLib.getVariable('Build.BuildNumber');
 
+
+        if(buildDir){
+            return buildDir+path.sep+reportType+'_'+buildNumber+'.json';
+        }
         // If the agent variable above is not specified (e.g. in debug environment), tempDir is undefined and
         // tmpNameSync function falls back to a default temp directory.
-        return tmpNameSync({dir: tempDir, prefix: 'cxreport-', postfix: '.json'});
+        switch (reportType) {
+            case TaskRunner.REPORT_ATTACHMENT_NAME:
+                return tmpNameSync({dir: buildDir, prefix: 'cxreport-', postfix: '.json'});
+                break;
+            case TaskRunner.REPORT_SCA_PACKAGES:
+                return tmpNameSync({dir: buildDir, prefix: this.REPORT_SCA_PACKAGES, postfix: '.json'});
+                break;
+            case TaskRunner.REPORT_SCA_FINDINGS:
+                return tmpNameSync({dir: buildDir, prefix: this.REPORT_SCA_FINDINGS, postfix: '.json'});
+                break;
+            case TaskRunner.REPORT_SCA_SUMMARY:
+                return tmpNameSync({dir: buildDir, prefix: this.REPORT_SCA_SUMMARY, postfix: '.json'});
+            default:
+                return tmpNameSync({dir: buildDir, prefix: 'cxreport-', postfix: '.json'});
+                break;
+        }
+
     }
 
     private printHeader() {
